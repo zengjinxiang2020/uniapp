ba9661bb94ac7c0af21435e2becad085178d20c2
Gaoxs <704041637@qq.com>
2020-08-13 14:20:34 +0800

    增加调试订阅通知的按钮

--- pages/home/index.vue
+++ pages/home/index.vue
@@ -11,7 +11,6 @@
 		</view>
 		<view class="fixed-header-box"></view>
 		<button @click="testTemplate">测试模板消息</button>
-
 		<view class="slider-banner banner">
 			<swiper indicatorDots="true" v-if="banner.length > 0" autoplay circular>
 				<block v-for="(item, bannerIndex) in banner" :key="bannerIndex">
