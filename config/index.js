2723cab7364b2e3e5d2ec4f2cd941461a936cb95
xuwenbo <717567226@qq.com>
2020-09-13 10:54:28 +0800

    修改app地址

--- config/index.js
+++ config/index.js
@@ -1,5 +1,5 @@
 
 // export const VUE_APP_API_URL = 'http://natapp.xinxintuan.co/api';
 // export const VUE_APP_API_URL = 'https://wxapi.yixiang.co/api';
-export const VUE_APP_API_URL = 'http://192.168.31.223:8008/api';
+export const VUE_APP_API_URL = 'http://h5api.xinxintuan.co/api';
 export const VUE_APP_RESOURCES_URL = 'https://wx.yixiang.co/static';
