61f6b4c385793d5195367d6f351864edb71a99e8
Gaoxs <704041637@qq.com>
2020-09-15 15:57:25 +0800

    合并

--- config/index.js
+++ config/index.js
@@ -1,12 +1,8 @@
 
-<<<<<<< HEAD
 // export const VUE_APP_API_URL = 'http://natapp.xinxintuan.co/api';
 export const VUE_APP_API_URL = 'https://wxapi.yixiang.co/api';
 // export const VUE_APP_API_URL = 'http://192.168.31.223:8008/api';
 // export const VUE_APP_API_URL = 'https://h5api.xinxintuan.co/api';
 // export const VUE_APP_API_URL = 'https://h5api.xinxintuan.co/api';
-=======
-export const VUE_APP_API_URL = 'https://wxapi.yixiang.co/api';
->>>>>>> cd4be74b4e66d2c142be343c65dc2de48ad97bd8
 export const VUE_APP_RESOURCES_URL = 'https://wx.yixiang.co/static';
 
