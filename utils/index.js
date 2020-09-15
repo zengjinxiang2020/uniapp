1286ede581b9f658df8f681257137e49e1d3cf60
Gaoxs <704041637@qq.com>
2020-09-15 16:28:48 +0800

    修改util编译错误的问题

--- utils/index.js
+++ utils/index.js
@@ -117,6 +117,8 @@ export function parseQuery() {
 	});
 	// #endif
 	// #ifndef H5
+	var pages = getCurrentPages() //获取加载的页面
+	var currentPage = pages[pages.length - 1] //获取当前页面的对象
 	var url = currentPage.route //当前页面url
 	res = currentPage.options //如果要获取url中所带的参数可以查看options
 	// #endif
